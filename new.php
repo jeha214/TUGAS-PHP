166d3eafe3382792ceaef00bac7a0e2b4891ba4d
jeha214 <95992417+jeha214@users.noreply.github.com>
2022-09-28 10:20:34 +0700

    Add files via upload

---
+++ new.php
@@ -0,0 +1,37 @@
+<?php
+$NamaLengkap = "Qoriatu Fajar";
+$nim = "20252031";
+$Alamat = "Candingasinan, Banyuurip";
+$Email = "qoriatufajar@gmmail.com";
+$Hobi = "Membaca";
+?>
+<table>
+<tr>
+<td><?php echo "Nama Lengkap";?>
+<td><?php echo ":"; ?></td>
+<td><?php echo $NamaLengkap ?></td>
+<td rowspan="9">
+<img src="foto.JPG" width="130" height="200">
+</td>
+</tr>
+<tr>
+<td><?php echo "Nim"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $nim ;?></td>
+</tr>
+<tr>
+<td><?php echo "Alamat"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $Alamat ;?></td>
+</tr>
+<tr>
+<td><?php echo "Email"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $Email ;?></td>
+</tr>
+<tr>
+<td><?php echo "Hobi"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $Hobi ;?></td>
+</tr>
+</table>
